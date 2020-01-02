ec0e546571d80a2c814cc4b13f68578cc5c97820
Morgan Styers <morganstyers@gmail.com>
2020-01-02 18:57:27 -0500

    hahahagahahah

--- compendium/src/App.js
+++ compendium/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        this will be our app if it kills me
         </p>
         <a
           className="App-link"
