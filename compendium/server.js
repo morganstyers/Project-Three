559d35b107a2f011af33a3261c4e176420aec939
rachel doran <rmitchell0925@gmail.com>
2020-01-02 18:46:01 -0500

    ughhh

--- compendium/server.js
+++ compendium/server.js
@@ -15,8 +15,9 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// // Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Project-Three");
+
 
 // Start the API server
 app.listen(PORT, function() {
