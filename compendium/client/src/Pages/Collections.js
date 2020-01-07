c27e6ee0d0dd607b606191feaff5cf66549cb8fa
Morgan Styers <morganstyers@gmail.com>
2020-01-06 22:53:29 -0500

    proxy error fixed

--- compendium/client/src/Pages/Collections.js
+++ compendium/client/src/Pages/Collections.js
@@ -42,7 +42,7 @@ const collections = () => {
             <span>
                 <CardGroup>
                     <Card>
-                        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                        <CardImg top width="100%" src="" alt="Card image cap" />
                         <CardBody>
                             <CardTitle>Card title</CardTitle>
                             <CardSubtitle>Card subtitle</CardSubtitle>
@@ -57,7 +57,7 @@ const collections = () => {
                             <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
                             <AddMineral id="rocks" buttonLabel="Add Mineral" />
                             <br/>
-                            <RocksModal buttonLabel="View Collection"></RocksModal>
+                            <RocksModal buttonLabel="Gallery"></RocksModal>
                         </CardBody>
                     </Card>
                     <Card>
@@ -68,7 +68,7 @@ const collections = () => {
                             <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
                             <AddCoinModal id="coins" buttonLabel="Add Coin" />
                             <br/>
-                            <CoinsModal buttonLabel="View Collection"></CoinsModal>
+                            <CoinsModal buttonLabel="Gallery"></CoinsModal>
 
                         </CardBody>
                     </Card>
