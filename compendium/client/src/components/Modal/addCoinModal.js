8571287772f8cd6f448ac09ae1777dd7ba37b3e3
Morgan Styers <morganstyers@gmail.com>
2020-01-07 03:52:50 -0500

    Creation form. needs validation and routing as well as some fine tuning and conditional rendering

--- compendium/client/src/components/Modal/addCoinModal.js
+++ compendium/client/src/components/Modal/addCoinModal.js
@@ -2,120 +2,119 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-
 import React from 'react';
 
 function MyVerticallyCenteredModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-           Add New Coin
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add New Coin
           </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+      </Modal.Header>
+      <Modal.Body>
         <Form>
-                <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelect">Select</Label>
-                    <Input type="select" name="select" id="exampleSelect">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelectMulti">Select Multiple</Label>
-                    <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleText">Text Area</Label>
-                    <Input type="textarea" name="text" id="exampleText" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleFile">File</Label>
-                    <Input type="file" name="file" id="exampleFile" />
-                    <FormText color="muted">
-                        This is some placeholder block-level help text for the above input.
-                        It's a bit lighter and easily wraps to a new line.
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleSelect">Select</Label>
+            <Input type="select" name="select" id="exampleSelect">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleSelectMulti">Select Multiple</Label>
+            <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleText">Text Area</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleFile">File</Label>
+            <Input type="file" name="file" id="exampleFile" />
+            <FormText color="muted">
+              This is some placeholder block-level help text for the above input.
+              It's a bit lighter and easily wraps to a new line.
         </FormText>
-                </FormGroup>
-                <FormGroup tag="fieldset">
-                    <legend>Radio Buttons</legend>
-                    <FormGroup check>
-                        <Label check>
-                            <Input type="radio" name="radio1" />{' '}
-                            Option one is this and that—be sure to include why it's great
+          </FormGroup>
+          <FormGroup tag="fieldset">
+            <legend>Radio Buttons</legend>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}
+                Option one is this and that—be sure to include why it's great
+            </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}
+                Option two can be something else and selecting it will deselect option one
           </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                        <Label check>
-                            <Input type="radio" name="radio1" />{' '}
-                            Option two can be something else and selecting it will deselect option one
+            </FormGroup>
+            <FormGroup check disabled>
+              <Label check>
+                <Input type="radio" name="radio1" disabled />{' '}
+                Option three is disabled
           </Label>
-                    </FormGroup>
-                    <FormGroup check disabled>
-                        <Label check>
-                            <Input type="radio" name="radio1" disabled />{' '}
-                            Option three is disabled
-          </Label>
-                    </FormGroup>
-                </FormGroup>
-                <FormGroup check>
-                    <Label check>
-                        <Input type="checkbox" />{' '}
-                        Check me out
+            </FormGroup>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="checkbox" />{' '}
+              Check me out
         </Label>
-                </FormGroup>
-                <Button>Submit</Button>  
+          </FormGroup>
+          <Button>Submit</Button>
 
-            </Form>
+        </Form>
 
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-  function AddCoinModel() {
-    const [modalShow, setModalShow] = React.useState(false);
-  
-    return (
-      <ButtonToolbar>
-        <Button style={{marginLeft: 100}} variant="warning" onClick={() => setModalShow(true)}>
-     Add New Coin
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+function AddCoinModel() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <ButtonToolbar>
+      <Button style={{ marginLeft: 100 }} variant="warning" onClick={() => setModalShow(true)}>
+        Add New Coin
         </Button>
-    
-  
-        <MyVerticallyCenteredModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-        />
-      </ButtonToolbar>
-    );
-  }
-  
 
 
-  export default AddCoinModel;
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </ButtonToolbar>
+  );
+}
+
+
+
+export default AddCoinModel;
