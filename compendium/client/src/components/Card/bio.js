e4210b5c8bd4f020e27156e89d94f2dbbf0bcaf4
Morgan Styers <morganstyers@gmail.com>
2020-01-08 18:33:47 -0500

    hhhh

--- compendium/client/src/components/Card/bio.js
+++ compendium/client/src/components/Card/bio.js
@@ -5,18 +5,8 @@ function Bio() {
     return (
             <Card style={{ width: 700, marginLeft: 350, marginTop: 20 }} color="default">
                 <hr />
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                A compendium is a concise collection of information pertaining to a body of knowledge. A compendium may summarize a larger work. In most cases the body of knowledge will concern a specific field of human interest or endeavour, while a general encyclopedia can be referred to as a compendium of all human knowledge.
+                We Have created a way to have that at your fingertips.
                 <hr/>
         </Card>
     );
