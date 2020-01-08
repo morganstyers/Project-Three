e5ea4169234e596967c207dba7f469ad835751a0
rachel doran <rmitchell0925@gmail.com>
2020-01-08 18:14:46 -0500

    added about US

--- compendium/client/src/components/Card/bio.js
+++ compendium/client/src/components/Card/bio.js
@@ -5,18 +5,10 @@ function Bio() {
     return (
             <Card style={{ width: 700, marginLeft: 350, marginTop: 20 }} color="default">
                 <hr />
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                <p>Morgan Styers - cat lover, free thinker, lover of all things art.</p>
+                <p>Rachel Doran - also a cat lover, vegetarian, music enthusiast</p>
+                <p>Brian Yeager - down to earth dude, dog lover, sagittarius</p>
+
                 <hr/>
         </Card>
     );
