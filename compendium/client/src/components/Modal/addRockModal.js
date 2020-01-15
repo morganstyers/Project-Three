2e63097ebedccd842df9bfef2580ad5aaf3c8008
Brian Yeager <rbrianyeager@gmail.com>
2020-01-14 19:12:13 -0500

    blah

--- compendium/client/src/components/Modal/addRockModal.js
+++ compendium/client/src/components/Modal/addRockModal.js
@@ -20,10 +20,7 @@ function MyVerticallyCenteredModal(props) {
         </Modal.Header>
         <Modal.Body>
         <Form>
-                <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                </FormGroup>
+                
                 <FormGroup>
                     <Label for="examplePassword">Password</Label>
                     <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
